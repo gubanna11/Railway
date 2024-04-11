78d27b7cba89b73e191b16f22b20282eee416ad2
Hanna Huba <gubanna.11@gmail.com>
2024-04-11 15:08:16 +0300

    Add UnitOfWork and GenericRepository implementations

---
+++ Railway.API/Railway.Infrastructure/Abstract/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Railway.Infrastructure.Abstract.Interfaces;
+using Railway.Infrastructure.Data;
+using Railway.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Railway.Infrastructure.Abstract;
+
+public class GenericRepository<T> : IGenericRepository<T>
+    where T : BaseEntity
+{
+    private readonly AppDbContext _context;
+
+    public IQueryable<T> Set { get; }
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        Set = context.Set<T>();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null,
+        Func<IQueryable<T>, IOrderedEnumerable<T>> orderBy = null,
+        params Expression<Func<T, object>>[] includeProperties)
+    {
+        IQueryable<T> query = Set;
+
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+
+        query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+        if (orderBy != null)
+        {
+            return orderBy(query).ToList();
+        }
+        else
+        {
+            return query.ToList();
+        }
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T?> RemoveAsync(int id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+
+        if (entity is not null)
+        {
+            _context.Set<T>().Remove(entity);
+            return entity;
+        }
+
+        return null;
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+
+    public void Update(T entity)
+    {
+        _context.Set<T>().Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+}
---
+++ Railway.API/Railway.Infrastructure/Abstract/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Railway.Infrastructure.Entities;
+using System.Threading.Tasks;
+
+namespace Railway.Infrastructure.Abstract.Interfaces;
+
+public interface IUnitOfWork<T> where T : BaseEntity
+{
+    IGenericRepository<T> GenericRepository { get; }
+
+    Task SaveAsync();
+}
