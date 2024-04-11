b62fa75a27f76b801f1651fbd75d59e20689b50c
Huba Hanna <gubanna.11@gmail.com>
2024-04-11 15:09:21 +0300

    Add UnitOfWork and GenericRepository implementations (#2)

---
+++ Railway.API/Railway.Infrastructure/Abstract/Interfaces/IGenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Railway.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Railway.Infrastructure.Abstract.Interfaces;
+
+public interface IGenericRepository<T> 
+    where T : BaseEntity
+{
+    IQueryable<T> Set { get; }
+
+    Task<IEnumerable<T>> GetAllAsync();
+
+    IEnumerable<T> GetAll(
+           Expression<Func<T, bool>> filter = null,
+           Func<IQueryable<T>, IOrderedEnumerable<T>> orderBy = null,
+           params Expression<Func<T, object>>[] includeProperties);
+
+    Task<T?> GetByIdAsync(int id);
+
+    Task<T> AddAsync(T entity);
+
+    void Update(T entity);
+
+    Task<T?> RemoveAsync(int id);
+
+    void RemoveRange(IEnumerable<T> entities);
+}
+
